d7674d0539eff19ea62c9a46e6c415b0db0b1d0f
jarsba <jarkko.lehtiranta@selko.io>
2019-11-16 21:10:59 +0200

    new try

--- its-a-jungle-api/models/Issue.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const issueSchema = new Schema({
-    name: String,
-    description: String,
-})
-
-mongoose.model('issues', issueSchema);
